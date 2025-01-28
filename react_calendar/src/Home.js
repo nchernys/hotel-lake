e53e9d5f8ddc6fe7c7ccb30260989e2c08358bbb
Chernysheva <natasha.e.chernysheva@gmail.com>
2025-01-27 22:38:25 -0500

    updated home image and animation

--- react_calendar/src/Home.js
+++ react_calendar/src/Home.js
@@ -1,54 +1,78 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
 import { gsap } from "gsap";
+import { ReactComponent as Blob } from "./icons/blobs.svg";
 
 const Home = () => {
   const [isVisible, setIsVisible] = useState(false);
   const wordsRef = useRef([]);
   const btnRef = useRef();
+  const bgRef = useRef();
 
   useEffect(() => {
-    // Animate each word one by one
-    gsap.fromTo(
-      wordsRef.current,
-      {
-        opacity: 0,
-        y: 20,
-      },
-      {
-        opacity: 1, // Fade in
-        y: 0, // Move to original position
-        stagger: 0.5, // Delay between words
-        duration: 1, // Duration for each word
-        ease: "power2.out",
-      }
-    );
-    gsap.fromTo(
-      btnRef.current,
-      {
-        opacity: 0,
-        y: 20,
-      },
-      {
-        opacity: 1,
-        y: 0,
-        duration: 0.5,
-        delay: 1.5, // Start after the words animation
-        ease: "power2.out",
-      }
-    );
+    gsap.set([...wordsRef.current, btnRef.current, bgRef.current], {
+      opacity: 0,
+    });
+    if (bgRef.current && wordsRef.current) {
+      gsap.fromTo(
+        bgRef.current,
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1, // Fade in
+          duration: 0.8, // Duration for each word
+          ease: "power2.out",
+        }
+      );
+      gsap.fromTo(
+        wordsRef.current,
+        {
+          opacity: 0,
+          y: 20,
+        },
+        {
+          opacity: 1, // Fade in
+          y: 0, // Move to original position
+          stagger: 0.5, // Delay between words
+          duration: 1, // Duration for each word
+          ease: "power2.out",
+          delay: 1.5,
+        }
+      );
+      gsap.fromTo(
+        btnRef.current,
+        {
+          opacity: 0,
+          scaleX: 0.7,
+        },
+        {
+          opacity: 1,
+          scaleX: 1,
+          duration: 2,
+          delay: 3.3, // Start after the words animation
+          ease: "elastic.out",
+        }
+      );
+    }
   }, []);
 
   return (
     <>
-      <div className="home-image w-full flex flex-col justify-center p-5 md:p-[5rem] lg:p-[10rem] lg:flex-row items-center relative z-1">
+      <div className="home-image w-full h-[calc(99.5vh-3rem)] flex flex-col justify-center p-5 md:p-[5rem] lg:p-[10rem] lg:flex-row items-center relative z-1 overflow-hidden">
+        <div
+          ref={bgRef}
+          className="absolute w-full left-0 top-0 overflow-hidden"
+        >
+          <img src="./images/hotel/hotel-home-5.webp" alt="home-hotel-image" />
+        </div>
         <div className="absolute left-0 top-0 w-full h-full bg-sky-600 opacity-40 sm:opacity-20 lg:opacity-10 z-0"></div>
         <div className="flex flex-col w-full ps-5 text-7xl font-bold text-white md:w-1/2 md:items-center z-10">
           {["Mountain", "Lake", "Resort"].map((word, index) => (
             <span
               key={index}
               ref={(el) => (wordsRef.current[index] = el)}
-              className="font-meaCulpa font-extralight text-7xl xs:text-8xl sm:text-[10rem] sm:font-medium my-2 sm:my-4"
+              className="font-meaCulpa font-extralight text-7xl xs:text-8xl sm:text-[10rem] sm:font-medium my-2 sm:my-4 opacity-0"
             >
               {word}
             </span>
@@ -62,11 +86,7 @@ const Home = () => {
             className="text-4xl font-serif font-semibold text-white md:flex md:justify-center relative overflow-visible xs:text-5xl sm:text-5xl"
             to="/choose-room"
           >
-            <img
-              className="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 min-w-44 w-44 h-auto -z-1 block xs:w-60 xs:min-w-60 md:w-72 md:min-w-72"
-              src="./icons/wave-bg-3.svg"
-              alt="wave-background"
-            />
+            <Blob className="absolute w-full h-auto left-[50%] top-[50%] transform -translate-x-[50%] -translate-y-[50%]" />
             <span className="z-10 relative">Book</span>
           </Link>
         </div>
